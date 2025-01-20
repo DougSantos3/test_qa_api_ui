f464eb1d04e1f6ff6741c0d5b05508d49f1fab6d
douglas <douglasmartinssantos7@gmail.com>
2025-01-20 10:18:47 -0300

    chore: config mult enviroments

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,41 @@
 const { defineConfig } = require('cypress')
-const { allureCypress } = require('allure-cypress/reporter');
-
-
+const { allureCypress } = require('allure-cypress/reporter')
 const cyPostgres = require('cypress-postgres-10v-compatibility')
+
+function getBaseUrls() {
+  const env = process.env.NODE_ENV || 'qa'
+  return {
+    dev: {
+      ui: '',
+      api: '',
+    },
+    qa: {
+      ui: 'https://front.serverest.dev',
+      api: 'https://serverest.dev/',
+    },
+    prod: {
+      ui: '',
+      api: '',
+    },
+  }[env]
+}
+
+const baseUrls = getBaseUrls()
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureCypress(on, config)
+      allureCypress(on, config, {})
       on('task', {
         dbQuery: (query) => cyPostgres(query.query, query.connection),
       })
-
       return config
     },
-    baseUrl: 'https://serverest.dev/',
+    baseUrl: baseUrls.api,
     env: {
-      baseUrlFront: 'https://front.serverest.dev',
+      baseUrlFront: baseUrls.ui,
     },
+    video: true,
     retries: {
       runMode: 0,
       openMode: 0,
