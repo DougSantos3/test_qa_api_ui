36eff5c6cb7367f7a13ea0daaeb5f769d12fb6ac
douglas <douglasmartinssantos7@gmail.com>
2025-01-20 11:22:03 -0300

    chore: information about end in report allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 const { allureCypress } = require('allure-cypress/reporter')
 const cyPostgres = require('cypress-postgres-10v-compatibility')
+const os = require('os')
 
 function getBaseUrls() {
   const env = process.env.NODE_ENV || 'qa'
@@ -20,12 +21,21 @@ function getBaseUrls() {
   }[env]
 }
 
+const env = process.env.NODE_ENV || 'qa'
 const baseUrls = getBaseUrls()
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureCypress(on, config, {})
+      allureCypress(on, config, {
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+          environment: env,
+        },
+      })
       on('task', {
         dbQuery: (query) => cyPostgres(query.query, query.connection),
       })
