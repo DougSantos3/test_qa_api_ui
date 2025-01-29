28f6e2e2beeca18bab9c5717102694f2eacb078a
douglas <douglasmartinssantos7@gmail.com>
2025-01-29 12:45:40 -0300

    fix: report allure

--- cypress.config.js
+++ cypress.config.js
@@ -8,16 +8,16 @@ const env = process.env.NODE_ENV || 'qa'
 function getBaseUrls() {
   return {
     dev: {
-      ui: '',
-      api: '',
+      ui: 'https://example.url.dev',
+      api: 'https://example.url.dev',
     },
     qa: {
       ui: 'https://front.serverest.dev',
       api: 'https://serverest.dev/',
     },
     prod: {
-      ui: '',
-      api: '',
+      ui: 'https://example.url.prod',
+      api: 'https://example.url.prod',
     },
   }[env]
 }
@@ -27,27 +27,12 @@ const baseUrls = getBaseUrls()
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      
-      /* Todo: Today it works only with Electron. I need to understand why it works with 
-      Chrome and Edge without breaking in the second scenario, but it executes the third one, 
-      and Firefox breaks in the second for the full test execution. Also, why doesn't the 
-      browser appear in the Allure report?
-      
-      let browserName = 'unknown'
-
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        browserName = browser.name
-        return launchOptions
-      })  */
-
       allureCypress(on, config, {
         environmentInfo: {
           os_platform: os.platform(),
           os_release: os.release(),
           os_version: os.version(),
           node_version: process.version,
-          environment: env,
-          /* browser: browserName, */
         },
       })
 
